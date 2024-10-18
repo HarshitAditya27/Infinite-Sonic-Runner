1d0ee21f7d50d5325ff58faa69fdfc509855866f
Harshit Aditya <harshitaditya27@gmail.com>
2024-10-18 22:36:31 +0530

    Added initial setup

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import kaplay from "kaplay";
+
+import { k } from "./kaplayContext";
+
+// kaplay({
+//   width: 1920,
+//   height: 1080,
+//   letterbox: true,
+//   background: [0, 0, 0],
+// });
+
+k;
