e6d8be0f0a1b87e86ecce53578781edb721c6c66
Harshit Aditya <harshitaditya27@gmail.com>
2024-10-20 19:26:04 +0530

    installed game assets

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
-// import kaplay from "kaplay";
+import k from "./kaplayContext";
 
-import { k } from "./kaplayContext";
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("patforms", "graphics/patforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+  sliceX: 8,
+  sliceY: 2,
+  anims: {
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+k.loadSprite("ring", "graphics/ring.png", {
+  sliceX: 16,
+  sliceY: 1,
+  anims: {
+    spin: { from: 0, to: 25, loop: true, speed: 30 },
+  },
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+  sliceX: 5,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
+});
 
-// kaplay({
-//   width: 1920,
-//   height: 1080,
-//   letterbox: true,
-//   background: [0, 0, 0],
-// });
-
-k;
+k.loadFont("mania", "fonts/mania.ttf");
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/City.wav");
